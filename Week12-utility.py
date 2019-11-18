01871b52619b5ee08e32211b44d8cc3975a171ac
Michael Ortega <mike.ortegacodbo@gmail.com>
2019-11-18 15:11:10 -0700

    Added the UpdateString Function

--- Week12-utility.py
+++ Week12-utility.py
@@ -13,5 +13,18 @@ def PrintOutput(string):
 def LoadFile(file_name):
     open_file = open(file_name, 'r')
     contents = open_file.read().split('\n')
-    PrintOutput(contents)
     open_file.close()
+    return contents
+
+# UpdateString Function
+
+def UpdateString(string_1, string_2, i):
+    list_form = list(string_1)
+    list_form[i] = string_2
+    output = ''
+    for letter in list_form:
+        output += letter
+    PrintOutput(output)
+    
+
+
